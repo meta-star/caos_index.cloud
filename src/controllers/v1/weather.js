f1464228abd8f85b0eb0910e70ead0078e285353
SuperSonic <supersonic@livemail.tw>
2022-06-01 10:47:33 +0800

    Add cache to weather_ip

--- src/controllers/v1/weather.js
+++ src/controllers/v1/weather.js
@@ -14,31 +14,61 @@ const http_client = {
     }),
 };
 
-module.exports = (ctx, router) => {
-    feature_router.get('/ip', async (req, res) => {
-        const ip_addr = process.env.NODE_ENV !== 'development' ? ip_address(req) : '1.1.1.1';
-        const ip_data = await http_client.basic.get('basic/network/ip/geo', {params: {ip_addr}});
-        if (!ip_data?.data) {
-            res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
-            return;
-        }
-        const location_data = ip_data.data?.data?.location;
-        if (!location_data) {
-            res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
-            return;
-        }
-        const weather_data = await http_client.weather.get('2.5/weather', {
+async function _getIpLocation(ip_addr) {
+    try {
+        const ip_data = await http_client.basic
+            .get('basic/network/ip/geo', {
+                params: {ip_addr}
+            });
+        return ip_data?.data?.data?.location;
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
+
+async function _getWeatherData(location_data) {
+    try {
+        return await http_client.weather.get('2.5/weather', {
             params: {
                 appid: process.env.OPENWEATHERMAP_API_KEY,
                 lat: location_data.latitude,
                 lon: location_data.longitude,
             }
         });
-        res.send({
-            timestamp: ctx.now(),
-            network_timezone: location_data?.time_zone,
-            weather: weather_data.data
-        });
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
+
+module.exports = (ctx, router) => {
+    feature_router.get('/ip', async (req, res) => {
+        const ip_addr = process.env.NODE_ENV !== 'development' ? ip_address(req) : '1.1.1.1';
+        const cache_key = `weather_ip_${ip_addr}`;
+        if (ctx.cache.has(cache_key)) {
+            const result = ctx.cache.get(cache_key);
+            result.timestamp = ctx.now();
+            res.send(result);
+            return;
+        }
+        const location_data = await _getIpLocation(ip_addr);
+        if (!location_data) {
+            res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+            return;
+        }
+        const weather_data = await _getWeatherData(location_data);
+        if (!weather_data) {
+            res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+            return;
+        }
+        const result = {
+            weather: weather_data.data,
+            network_timezone: location_data?.time_zone
+        };
+        ctx.cache.set(cache_key, result, 60);
+        result.timestamp = ctx.now();
+        res.send(result);
     });
     router.use('/weather', feature_router);
 };
