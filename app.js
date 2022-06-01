5a58b8da178821c82cbb5f54f5d917d3fb24a3c1
SuperSonic <supersonic@livemail.tw>
2022-06-01 16:36:35 +0800

    Update

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/', (_, res) => {
 
 controllers.forEach((c) => c(ctx, app));
 
-console.log(`${constant.APP_NAME}\n====`);
+console.log(`${constant.APP_NAME} (runtime: ${process.env.RUNTIME_ENV || "native"})\n====`);
 require('./src/execute')(app, ({type, hostname, port}) => {
     const protocol = type === 'general' ? 'http' : 'https';
     console.log(`Protocol "${protocol}" is listening at`);
