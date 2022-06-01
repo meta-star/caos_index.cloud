bb0cd0f53feecf26b359dc6ce6289ca406908bff
SuperSonic <supersonic@livemail.tw>
2022-06-01 19:08:08 +0800

    Update

--- src/controllers/v1/automate.js
+++ src/controllers/v1/automate.js
@@ -4,7 +4,10 @@ const {Router} = require('express');
 const feature_router = Router();
 
 module.exports = (ctx, router) => {
-    feature_router.get('/', (_, res) => {
+    feature_router.get('/switch', (_, res) => {
+        res.sendStatus(StatusCodes.SERVICE_UNAVAILABLE);
+    });
+    feature_router.post('/switch', (_, res) => {
         res.sendStatus(StatusCodes.SERVICE_UNAVAILABLE);
     });
     router.use('/automate', feature_router);
--- src/controllers/v1/index.js
+++ src/controllers/v1/index.js
@@ -1,3 +1,4 @@
+const {StatusCodes} = require("http-status-codes");
 const {Router} = require('express');
 
 const router = Router();
@@ -8,6 +9,9 @@ const routes = [
 ];
 
 module.exports = (ctx, app) => {
+    router.get('/', (_, res) => {
+        res.send({status: StatusCodes.OK});
+    });
     routes.forEach((c) => c(ctx, router));
     app.use('/v1', router);
 };
