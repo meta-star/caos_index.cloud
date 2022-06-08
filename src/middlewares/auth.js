8bd9e0342a2847a33439eabe0d83d27c3ec57291
SuperSonic <supersonic@livemail.tw>
2022-06-08 16:17:14 +0800

    Fix auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,36 +1,13 @@
 "use strict";
-// Validate "Authorization" header, but it will not interrupt the request.
+// Validate "CAOS" header, but it will not interrupt the request.
 // To interrupt the request which without the request, please use "access.js" middleware.
-
-// Import auth_methods
-const auth_methods = {
-    "CAOS": async (ctx, req, _) => require('../utils/caos_token').validateAuthToken(ctx, req.auth_secret)
-};
 
 // Export (function)
 module.exports = (ctx) => function (req, res, next) {
-    const auth_code = req.header('Authorization');
+    const auth_code = req.header('CAOS');
     if (!auth_code) {
-        next();
-        return;
+        req.authenticated = require('../utils/caos_token')
+            .validateAuthToken(ctx, req.auth_secret);
     }
-    const params = auth_code.split(" ");
-    if (params.length !== 2) {
-        next();
-        return;
-    }
-    req.auth_method = params[0];
-    req.auth_secret = params[1];
-    if (!(req.auth_method in auth_methods)) {
-        next();
-        return;
-    }
-    auth_methods[req.auth_method](ctx, req, res)
-        .then((result) => {
-            if (!req.authenticated) {
-                req.authenticated = result;
-            }
-            next();
-        })
-        .catch((error) => console.error(error));
+    next();
 };
