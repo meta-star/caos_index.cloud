2a19e93de839c4087bfd1c0bdd5be3375534b5b5
SuperSonic <supersonic@livemail.tw>
2022-06-08 16:34:32 +0800

    Fix bug in auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = (ctx) => function (req, _, next) {
     const auth_code = req.header('CAOS');
     if (auth_code) {
         req.authenticated = require('../utils/caos_token')
-            .validateAuthToken(ctx, req.auth_secret);
+            .validateAuthToken(ctx, auth_code);
     }
     next();
 };
